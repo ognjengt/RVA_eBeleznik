bf3e4326005483371a79f1c782bfa28a324c16ca
Ognjen Gatalo <ognjeng95@gmail.com>
2017-08-09 21:25:29 +0200

    Setupovao potrebne klase za Entity (AccessDB i Configuration), dodao foldere u odgovarajuce projekte

---
+++ EBeleznik/Server/Access/AccessDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Access
+{
+    public class AccessDB : DbContext
+    {
+        public AccessDB() : base("dbConnection2015") { }
+    }
+}
